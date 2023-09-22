b853daaa3722a37652f19009ed5d0c811ab6bfd6
souro18 <sourajit.paul61@gmail.com>
2023-09-22 19:32:47 +0530

    init project

---
+++ src/Counter.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+const Counter = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(0);
+
+  const onStart = () => {
+    const t = setInterval(() => {
+      setTime((t) => t + 1);
+    }, 1000);
+    setIsRunning(true);
+    setTimer(t);
+  };
+
+  const onPause = () => {
+    setIsRunning(false);
+    if (timer) {
+      clearInterval(timer);
+      setTimer(0);
+    }
+  };
+
+  const onReset = () => {
+    onPause();
+    setTime(0);
+  };
+
+  return (
+    <div>
+      <div>{time}</div>
+      {isRunning ? (
+        <div>
+          <button onClick={onPause}>Pause</button>
+          <button onClick={onReset}>Reset</button>
+        </div>
+      ) : (
+        <button onClick={onStart}>Start</button>
+      )}
+    </div>
+  );
+};
+
+export default Counter;
